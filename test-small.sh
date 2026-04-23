5c8bc6a882275a183baccc2a1d3b802ee2b2debc
shanzhou <shan.zhou@intel.com>
2026-04-23 03:30:53 +0000

    update comment

--- test-small.sh
+++ test-small.sh
@@ -1,1 +1,7 @@
-python summarize_to_retrieved_context_sglang.py   --output_jsonl ./data/longbench_v2_with_rc.jsonl   --api_key EMPTY   --model Qwen3-1.7B-local/
+python pred-opt.py \
+  --model Qwen3-30B-A3B-local \
+  --url http://127.0.0.1:30000/v1 \
+  --api_key EMPTY \
+  --input_jsonl data/longbench_v2_with_rc_1.7B.jsonl \
+  --save_dir results/run_1_7b \
+  --prompt_token_margin 512
